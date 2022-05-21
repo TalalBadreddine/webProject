df9b5b11339a4af9f3c4f97d2f2934e22f329201
talal <talalbadreddine@hotmail.com>
2022-05-21 12:41:26 +0300

    SecondRegisterPage Fixed

--- WWW/Student/Register/SecondRegistPage/Js/secondRegister.js
+++ WWW/Student/Register/SecondRegistPage/Js/secondRegister.js
@@ -1,89 +1,148 @@
-const language = document.getElementById("Language")
-const languageSpan = document.getElementById("LanguageSpan")
+const gender = document.getElementById("Gender")
+const language = document.getElementById('langugeId')
+const bloodType = document.getElementById('BloodType')
+const status = document.getElementById('statusId')
+const branch = document.getElementById("branchId")
+const dob = document.getElementById('dob')
+const address = document.getElementById('address')
+const phoneNumber = document.getElementById('phone')
+const schoolGrades = document.getElementById('schoolGrades')
+const schoolGradesSpan = document.getElementById('schoolSpan')
+const personalPhoto = document.getElementById('personalPhoto')
+const personalPhotoSpan = document.getElementById('personalPhotoSpan')
+const idOrPassport = document.getElementById('idOrPassport')
+const idOrPassportSpan = document.getElementById('idOrPassportSpan')
 
-const major = document.getElementById("Major")
-const majorSpan = document.getElementById("MajorSpan")
+const submitBtn = document.getElementById('continueBtn')
 
-const branch = document.getElementById("Branch")
-const branchSpan = document.getElementById("BranchSpan")
+let arr = {gender, language, bloodType, status, branch, dob, address, phoneNumber,personalPhoto, schoolGrades,idOrPassport}
 
-const schoolGrades = document.getElementById("schoolGrades")
-const schoolGradesSpan = document.getElementById("schoolGradesSpan")
+ function validate(){
+     if(
+        validateField(branch, "Branch") &&
+         validateField(language, "Language") &&
+         validateField(bloodType, "BloodType") &&
+         validateField(status, "Status") &&
+         validateField(gender, "Gender") &&
+         validateDob() &&
+         validateField(address, "")&&
+         validatePhoneNumber()&&
+         validateFile(schoolGrades,schoolGradesSpan)&&
+         validateFile(personalPhoto,personalPhotoSpan)&&
+         validateFile(idOrPassport,idOrPassportSpan)
+     ){
+         window.location.href = '../../ThirdRegisterPage/html/thirdRegisterPage.html'
+     }
 
-const personalPhoto = document.getElementById("personalPhoto")
-const personalPhotoSpan = document.getElementById("personalPhotoSpan")
-
-const passport = document.getElementById("passport")
-const passportSpan = document.getElementById("passportSpan")
-
-language.addEventListener("click", reset())
-
-function reset(){
-    languageSpan.innerHTML = ""
 }
 
-function nextPage(){
-
-    if(validateLanguage() && validateMajor()  && validateBranch() && validateSchoolGrader() && validatePersonalPhoto()  && validatePassport()){
-        return true
-    }
-
-    return false
-}
-
-function validatePassport(){
-    if(passport.value == ""){
-        passportSpan.innerHTML = "Please select the passport"
+function validateFile(Dom, spanDom){
+    let value = Dom.value
+   
+    if(value == ""){
+        applyAnimation(spanDom)
         return false
     }
 
     return true
 }
 
-function validatePersonalPhoto(){
-    if(personalPhoto.value == ""){
-        personalPhotoSpan.innerHTML = "Please select the personal Photo"
+function validateDob(){
+    let value = dob.value
+
+    if(value == ""){
+        applyAnimation(dob)
+    }
+
+    let date = new Date()
+
+    let currentYear = date.getFullYear()
+
+    let year = value.split('-')[0]
+
+    if(year > currentYear - 15){
+        applyAnimation(dob)
         return false
     }
-    return true
+
+    return true;
 }
 
-function validateSchoolGrader(){
-    if(schoolGrades.value == ""){
-        schoolGradesSpan.innerHTML = "Please select the school Grades file"
+function resetMe(Id){
+    let element = document.getElementById(Id)
+    element.style.border = "0.2px solid black"
+    element.style.animation = "none"
+}
+
+function validatePhoneNumber(){
+    let value = phoneNumber.value
+    let arrOfValue = value.split('-')
+
+    if(arrOfValue[0].length == value.length ){
+        applyAnimation(phoneNumber)
         return false
     }
-    return true
-}
 
-function validateBranch(){
-    if(branch.value == ""){
-        branchSpan.innerHTML = "Please select the branch"
+    for(let i = 0 ; i < arrOfValue.length ; i++){
+        if(isNaN(arrOfValue[i])){
+            applyAnimation(phoneNumber)
+            return false
+        }
+    }
+
+    if(value == ""){
+        applyAnimation(phoneNumber)
         return false
     }
-    return true
-}
 
-function validateMajor(){
-    if(major.value == ""){
-        majorSpan.innerHTML = "Please select the major"
+    if(arrOfValue[0].length != 2 || arrOfValue[1].length != 3 || arrOfValue[2].length != 3){
+        applyAnimation(phoneNumber)
         return false
     }
+
     return true
 }
 
-function validateLanguage(){
-    if(language.value == ""){
-        languageSpan.innerHTML = "Please Select the language"
-        return false 
+function applyAnimation(DomOject){
+    DomOject.style.animation = 'wiggle 1.1s';
+    DomOject.style.border = " 1.7px solid red";
+
+}
+
+function validateField(dom, value){
+    return dom.value != value  || applyAnimation(dom)
+}
+
+phoneNumber.addEventListener('keyup', function(){
+    
+    if(phoneNumber.value.length == 2){
+        phoneNumber.value += '-'
     }
-    return true
-}
 
-function resett(id, spanId){
-    let element = document.getElementById(id)
-    let elementSpan = document.getElementById(spanId)
+    if(phoneNumber.value.length == 6){
+        phoneNumber.value += '-'
+    }
 
-    elementSpan.border = "1px solid black";
-    elementSpan.style.display = "none";
-}
+    if(phoneNumber.value.length >= 10){
+        phoneNumber.value =  phoneNumber.value.slice(0,10)
+    }
+
+})
+
+// File Name
+
+$(document).ready(function(){
+
+    $('#idOrPassport').change(function () {
+      $('#idOrPassportSpan').text(this.files[0].name + " file is selected");
+      schoolSpan.style.border = "1px solid black"
+    });
+
+    $('#personalPhoto').change(function () {
+        $('#personalPhotoSpan').text(this.files[0].name + " file is selected");
+      });
+
+    $('#schoolGrades').change(function () {
+        $('#schoolSpan').text(this.files[0].name + " file is selected");
+      });
+});

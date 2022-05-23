207bf94d77137fddcc4f7b704eaca952af84e28a
talal <talalbadreddine@hotmail.com>
2022-05-23 12:31:53 +0300

    branches in second register page

--- WWW/Student/Register/SecondRegistPage/php/getBranches.php
+++ WWW/Student/Register/SecondRegistPage/php/getBranches.php
@@ -2,5 +2,18 @@
 
 session_start();
 
-echo json_encode($_SESSION['branches']);
+$targetMajor = $_SESSION['program'];
+$branches = $_SESSION['branches'];
+
+$arr = array();
+
+
+for($x = 0 ; $x < count($branches) ; $x++){
+    if($branches[$x]['MajorName'] == $targetMajor){
+        array_push($arr,$branches[$x]['BranchName']);
+    }
+}
+
+echo json_encode($arr);
+
 ?>

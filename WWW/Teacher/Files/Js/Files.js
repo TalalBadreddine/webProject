28f6444e53667024862a5572f3159bac0733ecdd
talal <talalbadreddine@hotmail.com>
2022-05-29 23:41:12 +0300

    load data from file

--- WWW/Teacher/Files/Js/Files.js
+++ WWW/Teacher/Files/Js/Files.js
@@ -1,35 +1,104 @@
+const scroller = document.getElementById("scroller")
+const CourseName = document.getElementById("CourseName")
+
 
 function move(p1, p2){
-    var value = 80
+    var value = 130
     var scrollDirection = p2 == 'left'? -value : value
 
-    var element = document.getElementById("scroller")
+    var element = scroller
 
     element.scrollLeft += scrollDirection
 
 }
 
+var count = 0 ;
+
 document.getElementById('examsCheat').addEventListener('click', function(){
   window.location.href = ("../Html/studentTable.html")
 })
 
-function changBackGround(p1){
+function getCurrentCourseSettings(p1){
+    count = 0;
     var element = document.getElementById(p1)
+    let allCourses = document.getElementsByClassName("scrollerElement")
 
-    for(var i = 1; i < 11 ; i++){
+    for(var i = 1; i < allCourses.length ; i++){
         var targetId = "scrollerElement" + i
         var elementToUnbold = document.getElementById(targetId)
-        elementToUnbold.style.fontWeight = "normal";
+        if(elementToUnbold != undefined){
+         
+          elementToUnbold.style.fontWeight = "normal";
+        }
     }
 
     element.style.fontWeight = "bolder"
 
-    var scrollerPos = document.getElementById("scroller")
+    var scrollerPos = scroller
     var valueOfScroll = element.offsetLeft
 
     scrollerPos.scrollLeft = valueOfScroll - scrollerPos.clientWidth - scrollerPos.clientWidth/4 
-}
+    CourseName.innerHTML = element.innerHTML
 
+    $.ajax({
+      url:'../php/loadAllFiles.php',
+      type:'POST',
+      data:{
+        courseName: element.innerHTML
+      },
+      success:function(response){
+        console.log(fromCourseNameToId)
+        let data = JSON.parse(response)
+        console.log(data)
+        let filesHtml = ''
+        for(let x in data){
+
+          let fileName = data[x]
+          let iconClass =''
+          let currentArr = fileName.split('.')
+
+          if(currentArr.length > 1){
+            iconClass ='pdfIcon ' + filesIcons[currentArr[currentArr.length - 1]]
+          }else{
+            iconClass = 'pdfIcon fas fa-file-alt'
+           
+          }
+
+          filesHtml += `
+                <div class="filesRow ${count} , ${fileName} " id="row ${count}">
+                         <li class="fileItems item list-group-item ${data[x]}" id="filesItem${count}"><a href="#"></a><i class='${iconClass}'></i><span class="fileName">${fileName}</span><span class="deleteBtnSpan ${count}"><button class="deleteBtn" onclick="deleteMe('${count}')">delete</button></span><span
+                class="date">${date.getDate()} ${monthNames[date.getMonth()]}</span></li>
+                </div>
+              `
+              count++;
+        }
+
+        filesUlDiv.innerHTML = filesHtml
+
+        filesUlDiv = document.getElementById("files")
+        fileDivs = document.getElementsByClassName("filesRow")
+        for (let i = 0; i < fileDivs.length; i++) {
+
+          fileDivs[i].addEventListener('contextmenu',function(){
+            let currentPath = `${fromCourseNameToId[CourseName.innerHTML]}/${fileDivs[i].className.split(',')[1].trim()}`
+            window.open(`../../../../../../webProjectFiles/Courses/${currentPath}`)
+          })
+
+          // show the button
+           fileDivs[i].addEventListener('dblclick', function() {
+            showingDeleteBtn(fileDivs[i].id)
+          });
+}
+        
+      }
+    })
+
+
+}
+{/* <div class="filesRow 3" id="row 3">
+<li class="fileItems item list-group-item" id="filesItem3"><a href="#"></a><i class="pdfIcon fas fa-file-alt"></i><span class="fileName">file3.pdf</span><span class="deleteBtnSpan 3"><button class= "deleteBtn" onclick="deleteMe('3')">delete</button></span><span
+    class="date">5 Feb</span></li>
+</div> */}
 
 // Long click function
 // Now its a double click
@@ -37,11 +106,15 @@ function changBackGround(p1){
 var pressTimer;
 
 const deleteButtonDiv = document.getElementById("hiddenDelete")
-const filesUlDiv = document.getElementById("files")
-const fileDivs = document.getElementsByClassName("filesRow")
+var filesUlDiv = document.getElementById("files")
+var fileDivs = document.getElementsByClassName("filesRow")
 var posArr ;
 
 for (let i = 0; i < fileDivs.length; i++) {
+
+  fileDivs[i].addEventListener('click',function(){
+    window.open = `../../../../../../webProjectFiles/Courses/${fileDivs[i].split(',')[1]}`
+  })
 
   // show the button
   fileDivs[i].addEventListener('dblclick', function() {
@@ -65,74 +138,7 @@ function showingDeleteBtn(p1){
 }
 
 
-/*$(".fileItems").dblclick(function(e){
-//   clearTimeout(pressTimer);
-//   // Clear timeout
-//   return false;
-// }).mousedown(function(e){
-//   // Set timeout
-//   pressTimer = window.setTimeout(function() { 
-      console.log(this.clientY)
-      let currentItem = e
-      let currentId = currentItem.currentTarget.id
-
-      let deleteXPos = currentItem.clientX + "px"
-      let deleteYPos = parseInt(currentItem.clientY)+ "px"
-
-      // let deleteXPos = this.offsetLeft
-      // let deleteYPos
-        
-      deleteButtonDiv.style.zIndex = "43"
-      deleteButtonDiv.style.left = deleteXPos
-      deleteButtonDiv.style.top =  deleteYPos
-      deleteButtonDiv.style.border = "1px solid black"
-      deleteButtonDiv.style.borderRadius = "12px"
-      deleteButtonDiv.style.backgroundColor = "red"
-      deleteButtonDiv.style.color = "white"
-      deleteButtonDiv.style.width = "70px"
-      deleteButtonDiv.style.height = "30px"
-      deleteButtonDiv.style.display = "block"
-
-      
-      function getMouseXPos(event) {
-        return event.clientX 
-      }
-
-      function getMouseYPos(event) {
-        return event.clientY
-      }
-      
-        
-    document.addEventListener("click", function(e){
-        let currentX = getMouseXPos(e)
-        let currentY = getMouseYPos(e)
-
-        //|| currentY < parseInt(deleteYPos)  || currentY > parseInt(deleteYPos) + 31
-
-       if(currentX < parseInt(deleteXPos) || currentX > parseInt(deleteXPos)+71 ){
-        deleteButtonDiv.style.display = "none"
-       }
-    });
-
-      deleteButtonDiv.addEventListener('click',  function(){
-          console.log(currentItem.currentTarget)
-          currentItem.currentTarget.style.display = "none"
-         });
-         return false; 
-    })/*,600);*/
- 
-
-
-
-
-// function showPosition(e){
-
-//     var xPosition = e.clientX;
-//     var yPosition = e.clientY;
-
-// }
-// ----------------------
-
+// window.open('../../../../../../webProjectFiles/Student/talalbadreddine-628e64288dca8/personalPhoto');
 
 
 const files = document.getElementById("FileUpload1")
@@ -146,6 +152,12 @@ addCourseBtn.addEventListener('click', uploadClicked)
 addingToTableBtn.addEventListener('click', addFilesToTable)
 
 var allFiles = []
+
+var filesIcons={
+  'png':'fas fa-image',
+  'jpeg':'fas fa-image',
+  'mp4':'fas fa-video',
+}
 
 function uploadClicked(){
 
@@ -169,11 +181,6 @@ function addFilesToTable(){
         let fileArr = allFiles[i].split("\\")
         let fileName = fileArr[fileArr.length - 1];
 
-    //     <div class="filesRow" id="row1">
-    //     <li class="fileItems item list-group-item" id="filesItem1" data-long-press-delay="500"><a href="#"></a><i class="pdfIcon fas fa-file-alt"></i> file1.pdf<span
-    //         class="date">5 Feb</span></li>
-    //   </div>
-
         filesUlDiv.innerHTML  +=  `<div class ='filesRow ${startNumber+3}' id = 'row ${startNumber+3}'> <li class='fileItems item list-group-item' id='filesItem1'  data-long-press-delay='500'><a href='#'></a><i class='pdfIcon fas fa-file-alt'></i><span class='fileName'>${fileName}</span><span class='deleteBtnSpan ${startNumber + 3}'><button class='deleteBtn' onclick='deleteMe(${startNumber+3})' >delete</button></span><span class='date'>${date.getDate()} ${monthNames[date.getMonth()]}</span></li></div>`
         startNumber++
     }
@@ -194,4 +201,27 @@ function addFilesToTable(){
 function deleteMe(p1){
   let divToDeleteName = `row ${p1}`
   document.getElementById(divToDeleteName).style.display = "none"
+}
+
+// load All Courses From DB 
+var fromCourseNameToId = {}
+
+$(document).ready(function(){
+  $.ajax({
+    url:'../php/loadAllCourses.php',
+    type:'POST',
+    success:function(response){
+
+      let allCourses = JSON.parse(response)
+    
+      for(let i= 0 ; i < allCourses.length ; i++){
+        fromCourseNameToId[allCourses[i]["CourseName"]] = allCourses[i]["CourseID"]
+        addToScroller(allCourses[i])
+      }
+    }
+  })
+})
+
+function addToScroller(arr){
+  scroller.innerHTML += `<span class="scrollerElement" id="scrollerElement${arr["CourseID"]}" onclick="getCurrentCourseSettings('scrollerElement${arr["CourseID"]}')">${arr["CourseName"]}</span>`
 }
